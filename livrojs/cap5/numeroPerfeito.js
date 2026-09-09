60755b9dbd5ae488fa85a739295e744c22d74a58
Yasmim Kimberly <yasmimksdemelo@gmail.com>
2026-09-08 22:01:26 -0300

    Corrigindo o código

--- livrojs/cap5/numeroPerfeito.js
+++ livrojs/cap5/numeroPerfeito.js
@@ -1,18 +1,14 @@
 const prompt = require("prompt-sync")();
 const num = Number(prompt("Número: "));
-let soma = 1;
-let resul = [];
+let soma = 0;
 
 for (let i = 1; i < num; i++) {
-  if (num % i == 0) {
-    soma += num / i;
-    resul.push(num / i);
+  if (num % i == 0 || num % i == num) {
+    soma += i;
+    console.log(`Divisores do 28: ${i}`);
   }
 }
-for (let i = 1; i < resul.length; i++) {
-  if (resul[i] == num) {
-    resul.pop(num);
-  }
-  console.log(resul[i]);
-  console.log(typeof resul);
+if (soma != 0) {
+  console.log(`Soma: ${soma}`);
+  console.log(`${num} é um número perfeito`);
 }
